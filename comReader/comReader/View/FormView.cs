b4793e988bbadd181e0e9570716962c37a44adb9
suorness <inqire@gmail.com>
2017-10-29 23:33:57 +0300

    Проблемы с db

---
+++ comReader/comReader/View/FormView.cs
@@ -0,0 +1,19 @@
+﻿using readerLib.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace comReader.View
+{
+    class FormView : IView
+    {
+        public void ShowText(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
